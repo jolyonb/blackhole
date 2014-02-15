5c4d2b54c8671448f6b5726669e0ef724509120a
Stephen Face <face@mit.edu>
2014-02-15 01:38:48 -0500

    updating spect.c

--- spect.c
+++ spect.c
@@ -6,15 +6,15 @@ static double IWEIGHTS[N];
 static double TY[N+1];
 
 void mkwi(){
-  int i=N;
-  while(i-->0){
+  int i=N; while(i-->0){
+    //  for(i=0;i<N;i++){
     WEIGHTS[i]=1/(-2*N*sin(M_PI*(i+.5)/N));
     IWEIGHTS[i]=sin(M_PI*(i+.5)/N)/(-2*N);
   }
 }
 
 void plddx(dplan *d){
-  // plddx plans a differentiation operation on the data stored at d->y to output to d->dy
+  // plddx plans a differentiation operation on the data stored at d->yin to output to d->yout.
 
   const fftw_r2r_kind t[]={FFTW_REDFT10,FFTW_RODFT01};
   d->p[0]=fftw_plan_r2r_1d(N,d->yin,TY,t[0],FFTW_EXHAUSTIVE);
@@ -22,10 +22,10 @@ void plddx(dplan *d){
 }
 
 void plint(dplan *d){
-  // plint plans an integration operation on d->y to output to d->dy. Sorry oubout the name, I wrote the differentiater first.
+  // plint plans an integration operation on d->yin to output to d->yout.
   
   const fftw_r2r_kind t[]={FFTW_RODFT10,FFTW_REDFT01};
-  d->p[0]=fftw_plan_r2r_1d(N,d->yin,TY+1,t[0],FFTW_EXHAUSTIVE);
+  d->p[0]=fftw_plan_r2r_1d(N,d->yin,TY+1,t[0],FFTW_EXHAUSTIVE | FFTW_DESTROY_INPUT);
   d->p[1]=fftw_plan_r2r_1d(N,TY,d->yout,t[1],FFTW_EXHAUSTIVE | FFTW_DESTROY_INPUT);
 }
 
@@ -35,13 +35,16 @@ void exddx(dplan *d){
   //   => y' = sum(a_n n sin(n \theta) / sin(\theta))
 
 
-  double b=d->b;
-  fftw_execute(d->p[0]);
-
+  double b=d->b*N;
   int i;
-  i=N;
-  b*=N;
-  while(i-->1){
+
+  fftw_execute(d->p[0]);
+  
+  //Testing of these functions has shown reverse looping to be faster on i5-2450M. Commented out are the equivalent forward loops.
+
+  #pragma GCC ivdep
+  i=N; while(i-->1){
+  //for(i=1;i<N;i++){
     b+=TY[i]*(2*(i&1)-1);
     TY[i]*=i;
   }
@@ -49,10 +52,9 @@ void exddx(dplan *d){
   
   fftw_execute(d->p[1]);
 
-  i=N;
-
   #pragma GCC ivdep
-  while(i-->0){
+  i=N; while(i-->0){
+  //for(i=0;i<N;i++){
      d->yout[i]*=WEIGHTS[i];
   }  
 }
@@ -60,13 +62,14 @@ void exddx(dplan *d){
 void exddxl(dplan *d){
   //puts the boundary condition on the left.
   
-  double b=d->b;
+  double b=d->b*N;
+  int i;
+
   fftw_execute(d->p[0]);
 
-  unsigned int i;
-  i=N;
-  b*=N;
-  while(i-->1){
+  #pragma GCC ivdep
+  i=N; while(i-->1){
+  //  for(i=1;i<N;i++){
     b-=TY[i];
     TY[i]*=i;
   }
@@ -74,33 +77,30 @@ void exddxl(dplan *d){
   
   fftw_execute(d->p[1]);
 
-  i=N;
-
   #pragma GCC ivdep
-  while(i-->0){
+  i=N; while(i-->0){
+  //for(i=0;i<N;i++){
      d->yout[i]*=WEIGHTS[i];
   }  
 }
 
 void exint(dplan *d){
-  // executes a planed integration using the reverse of the technique in exddx.
+  // executes a planed integration using the reverse of the technique in exddx. Currently destroys data in yin. Perhaps this can be avoided with some fourier identity magic and a multiplication at the end instead of the begining.
 
-  unsigned int i;
+  int i;
   double b=d->b;
-  i=N;
-
-  //icc is much faster in this function. I think because it handles the following loop better.
-  //I changed my mind. I really don't know why it's faster. it [GCC] seems to vectorize the loops just fine.
 
   #pragma GCC ivdep
-  while(i-->0){
+  i=N; while(i-->0){
+  //for(i=0;i<N;i++){
     d->yin[i]*=IWEIGHTS[i]; // note: input is destroyed. don't think we care.
   }
       
   fftw_execute(d->p[0]);
 
-  i=N+1;
-  while(i-->2){
+  #pragma GCC ivdep
+  i=N+1; while(i-->2){
+  //  for(i=2;i<N+1;i++){
     TY[i]/=i;
     b+=2*TY[i]*(2*(i&1)-1);
   }
